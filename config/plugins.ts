5b86565852a70cb9f018908465f79bd28e6fc99f
unknown <reemsina2@gmail.com>
2025-07-01 12:35:27 +0300

    edit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET',"+CkMDzT7R/ZY2TPiW8CAYJM4qYv/sidNcVCm5E9U8vk="),
+    },
+  },
+});
