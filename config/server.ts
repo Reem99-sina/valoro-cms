d236bba9e0cd4d517a833c0a6130a199f16f8d1e
unknown <reemsina2@gmail.com>
2025-07-01 12:31:01 +0300

    edit

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,10 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", [
+      "b47cfb9b7ad87fe068fe4e90f5cdeeaba1ee7455ade895ab5711ba743f80575a",
+      "8c3aec4de919760b2eccccb46558525232b326c8a430169298381a364ffd4813",
+    ]),
   },
 });
